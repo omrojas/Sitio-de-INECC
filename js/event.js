cc0e648f7b78d14bf7ee2ca2366f265398839564
Omar Rojas <omar.ro@outlook.com>
2018-08-29 23:04:00 -0500

    Initial commit

---
+++ js/event.js
@@ -0,0 +1,23 @@
+document.getElementById('event-title').innerHTML += 'MÉXICO ANTE EL CAMBIO CLIMÁTICO';
+
+document.getElementById('event-subtitle').innerHTML += 'Tercer Encuentro Nacional';
+
+document.getElementById('event-date').innerHTML += '30 de abril de 2018 - 10 de mayo de 2018';
+
+document.getElementById('event-description').innerHTML += 'Groups other than the Convention and Protocol Bodies<br>Please note that this schedule of daily meetings of groups other than the Convention and Protocol bodies will not be reprinted during the session. Participants are kindly requested to retain their copies of this document,and to consult the CCTV monitors for changes or updates.';
+
+
+document.getElementById('event-program').innerHTML += '<h5>Programa </h5>';
+
+document.getElementById('program-content-title').innerHTML += '<h6>Participación de México  para lograr los compromisos globales </h6>';
+
+
+
+
+
+
+
+
+
+
+
