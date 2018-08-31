d9bc30e086fc561b2bfa125c31cbbffb4c7125c7
Omar Rojas <omar.ro@outlook.com>
2018-08-31 01:53:01 -0500

    new-event-version
    Cambié TODO el código del evento porque se estaba volviendo muy problemático.
    
    Hice todo muy rápido así que hay unos problemas de identación y no es totalmente mobile-friendly en algunos detalles. Sin embargo, para el propósito de entrega sujeto a cambios del día 31 de agosto hará el trabajo.

--- js/event.js
+++ js/event.js
@@ -4,7 +4,7 @@ document.getElementById('event-subtitle').innerHTML += 'Tercer Encuentro Naciona
 
 document.getElementById('event-date').innerHTML += '30 de abril de 2018 - 10 de mayo de 2018';
 
-document.getElementById('event-description').innerHTML += 'Groups other than the Convention and Protocol Bodies<br>Please note that this schedule of daily meetings of groups other than the Convention and Protocol bodies will not be reprinted during the session. Participants are kindly requested to retain their copies of this document,and to consult the CCTV monitors for changes or updates.';
+document.getElementById('event-description').innerHTML += 'El Encuentro Nacional es un espacio para tender puentes entre los diversos sectores de la sociedad y construir un diálogo sobre la situación actual, acciones y retos de México plasmados en la Sexta Comunicación Nacional de Cambio Climático.';
 
 
 document.getElementById('event-program').innerHTML += '<h5>Programa </h5>';
